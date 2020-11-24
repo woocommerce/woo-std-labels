2115fb156d1db5da32c424e613a2d0a2b8917a5a
Peter Fabian <peter.fabian.github@gmail.com>
2020-11-24 18:49:51 +0100

    Removed the debug messages.

--- index.js
+++ index.js
@@ -7,20 +7,6 @@ const githubLabelSync = require('github-label-sync');
 void async function () {
 	try {
 		const labelsFilePath = core.getInput('labels-path');
-		console.log('Files in root:');
-		await fs.readdir('.', (err, files) => {
-			files.forEach(file => {
-			  console.log(file);
-			});
-		  });
-		console.log('Files in .github:');
-		await fs.readdir('./.github/', (err, files) => {
-			files.forEach(file => {
-			  console.log(file);
-			});
-		});
-		
-
 		const content = await fs.readFile(labelsFilePath, 'utf8');
 		const labels = JSON.parse(content);
 
